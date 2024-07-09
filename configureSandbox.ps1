451c8e6524f8f2432768ee92633d76f66fbafc2e
timkrebs9 <timkrebs9@gmail.com>
2024-07-09 13:35:08 +0200

    chore: Configure sandbox and camera access in PowerShell script

---
+++ configureSandbox.ps1
@@ -0,0 +1,19 @@
+# Open Microsoft Edge with a specific URL (without kiosk mode)
+Start-Process "msedge.exe" -Wait -WindowStyle Maximized -ArgumentList "https://www.microsoft.com"
+
+# Install certificate
+$params = @{
+    Filepath = "C:\sandbox\<CERT-NAME>.cer"
+    CertStoreLocation = "Cert:\LocalMachine\Root"
+}
+Import-Certificate @params
+
+# Ensure that the sub-key is created if it doesn't exist
+if (-not (Test-Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy")) {
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -Force
+}
+# Set the policy to allow apps access to the camera
+New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsAccessCamera" -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsAccessCamera_ForceAllowTheseApps" -PropertyType MultiString -Force
+New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsAccessCamera_ForceDenyTheseApps" -PropertyType MultiString -Force
+New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsAccessCamera_UserInControlOfTheseApps" -PropertyType MultiString -Force
