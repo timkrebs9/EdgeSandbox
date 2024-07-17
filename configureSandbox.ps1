a3e3faee57f9de458202ae7f45161cb26be9c1b2
timkrebs9 <timkrebs9@gmail.com>
2024-07-17 13:19:30 +0200

    Update sandbox configuration during sandbox startup

--- configureSandbox.ps1
+++ configureSandbox.ps1
@@ -1,10 +1,28 @@
+# =======================================================================================================================================================================
+# Purpose : Sandbox configuration script for Windows 10/11
+#
+# DISCLAIMER: The sample scripts provided here are not supported under any Microsoft standard support program or service. 
+# All scripts are provided AS IS without warranty of any kind.
+# Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. 
+# The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. 
+# In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever 
+# (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) 
+# arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages. 
+# 
+# ======================================================================================================================================================================
+
+##############################################################
 # Install certificate
+##############################################################
 $params = @{
     Filepath = "C:\CertStore\<CERT-NAME>.cer"
     CertStoreLocation = "Cert:\LocalMachine\Root"
 }
 Import-Certificate @params
 
+##############################################################
+# Enable Camera and Microphone
+##############################################################
 # Ensure that the sub-key is created if it doesn't exist
 if (-not (Test-Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy")) {
     New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -Force
@@ -15,7 +33,9 @@ New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -N
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsAccessCamera_ForceDenyTheseApps" -PropertyType MultiString -Force
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsAccessCamera_UserInControlOfTheseApps" -PropertyType MultiString -Force
 
+##############################################################
 # Disable Powershell
+##############################################################
 $explorerKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
 if (-not (Test-Path $explorerKeyPath)) {
     New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies" -Name "Explorer" -Force
@@ -41,5 +61,7 @@ Stop-Process -Name explorer -Force
 Start-Process -WindowStyle hidden -Name "explorer.exe"
 Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine
 
+##############################################################
 # Open Microsoft Edge with a specific URL (without kiosk mode)
+##############################################################
 Start-Process "msedge.exe" -Wait -WindowStyle Maximized -ArgumentList "https://www.microsoft.com"
